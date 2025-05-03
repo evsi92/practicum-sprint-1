590e9cc6693d35680e262f8e00f8f0fbc23d16db
Evgeniia Simonova <evgeniia@Sergeis-MacBook-Air.local>
2025-05-03 13:18:47 -0400

    Changes in Task1

--- apps/temperature-api/src/main/java/com/example/temperatureapi/model/TemperatureResponse.java
+++ apps/temperature-api/src/main/java/com/example/temperatureapi/model/TemperatureResponse.java
@@ -1,4 +1,13 @@
 package com.example.temperatureapi.model;
 
 public class TemperatureResponse {
+
+    Float value;
+    String unit;
+    String timestamp;
+    String location;
+    String status;
+    Integer id;
+    String type;
+    String name;
 }
