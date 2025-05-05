be2e2a8bd149e22fbdf652f47263f668671d3a8d
Evgeniia Simonova <evgeniia@Sergeis-MacBook-Air.local>
2025-05-04 22:15:49 -0400

    Task#5 fixed response

--- apps/temperature-api/src/main/java/com/example/temperatureapi/controller/TemperatureController.java
+++ apps/temperature-api/src/main/java/com/example/temperatureapi/controller/TemperatureController.java
@@ -1,12 +1,14 @@
 package com.example.temperatureapi.controller;
 
 import com.example.temperatureapi.model.Sensor;
+import com.example.temperatureapi.model.TemperatureResponse;
 import com.example.temperatureapi.repository.SensorRepository;
 import com.example.temperatureapi.utils.TemperatureGenerator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDateTime;
 import java.util.Optional;
 
 @RestController
@@ -17,7 +19,7 @@ public class TemperatureController {
     private SensorRepository sensorRepository;
 
     @GetMapping
-    public ResponseEntity<Sensor> getCurrentTemperatureInLocation(@RequestParam String location) {
+    public ResponseEntity<TemperatureResponse> getCurrentTemperatureInLocation(@RequestParam String location) {
         if (location == null || location.isEmpty()) {
             return ResponseEntity.badRequest().body(null);
         } else {
@@ -26,17 +28,24 @@ public class TemperatureController {
     }
 
     @GetMapping("/{sensorId}")
-    public ResponseEntity<Sensor> getCurrentTemperatureBySensorId(@PathVariable Integer sensorId) {
+    public ResponseEntity<TemperatureResponse> getCurrentTemperatureBySensorId(@PathVariable Integer sensorId) {
         return handleQueryResult(sensorRepository.findById(sensorId));
     }
 
-    private ResponseEntity<Sensor> handleQueryResult(Optional<Sensor> optionalSensor) {
+    private ResponseEntity<TemperatureResponse> handleQueryResult(Optional<Sensor> optionalSensor) {
         if (optionalSensor.isPresent()) {
             Sensor sensor = optionalSensor.get();
             sensor.setValue(TemperatureGenerator.getTemperature());
-            return ResponseEntity.ok(sensor);
+            return ResponseEntity.ok(transform(sensor));
         } else {
             return ResponseEntity.notFound().build();
         }
+    }
+
+    private TemperatureResponse transform(Sensor sensor) {
+        return new TemperatureResponse(sensor.getValue(), sensor.getUnit(),
+                LocalDateTime.now().toString(), sensor.getLocation(),
+                sensor.getStatus(), sensor.getId(),
+                sensor.getType(), sensor.getName());
     }
 }
--- apps/temperature-api/src/main/java/com/example/temperatureapi/model/TemperatureResponse.java
+++ apps/temperature-api/src/main/java/com/example/temperatureapi/model/TemperatureResponse.java
@@ -1,5 +1,7 @@
 package com.example.temperatureapi.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class TemperatureResponse {
 
     Float value;
@@ -7,7 +9,24 @@ public class TemperatureResponse {
     String timestamp;
     String location;
     String status;
+
+    @JsonProperty("sensor_id")
     Integer id;
+
+    @JsonProperty("sensor_type")
     String type;
+
+    @JsonProperty("description")
     String name;
+
+    public TemperatureResponse(Float value, String unit, String timestamp, String location, String status, Integer id, String type, String name) {
+        this.value = value;
+        this.unit = unit;
+        this.timestamp = timestamp;
+        this.location = location;
+        this.status = status;
+        this.id = id;
+        this.type = type;
+        this.name = name;
+    }
 }
